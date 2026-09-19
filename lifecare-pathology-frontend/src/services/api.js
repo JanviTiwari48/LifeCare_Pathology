fd8ef20637804d176d59902e4073ae0987925faa
JanviTiwari48 <211301506+JanviTiwari48@users.noreply.github.com>
2026-09-19 16:29:38 +0530

    Connect frontend to production backend

--- lifecare-pathology-frontend/src/services/api.js
+++ lifecare-pathology-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api'
+    baseURL: 'https://lifecare-pathology-ksk9.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
